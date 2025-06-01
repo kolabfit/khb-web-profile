58e5a10915618b20e2ed11719d25b09e19944c7d
rakhagans37 <muhammadrakha3704@gmail.com>
2025-06-01 14:54:10 +0700

    fix: model members

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Member extends Model
 {
@@ -14,4 +15,13 @@ class Member extends Model
         'name',
         'photo',
     ];
+
+    public function getPhotoAttribute($value)
+    {
+        if (request()->routeIs('filament.*')) {
+            return $value;
+        } else {
+            return env('APP_URL') . Storage::url($value);
+        }
+    }
 }
